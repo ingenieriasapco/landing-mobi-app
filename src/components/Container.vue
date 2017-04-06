38df4b87899d1c00d5f11e04def2f6a3c7c7ad1d
khriztianmoreno <khriztianmoreno@gmail.com>
2017-04-06 14:43:55 -0500

    Create component form for reset password

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -4,30 +4,12 @@
     <h3>¿Has olvidado tu contraseña?</h3>
 
     <div class="form--container">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3 class="panel-title"><strong>Ingresa aquí una nueva para el acceso a tu aplicación</strong></h3>
-        </div>
-        <div class="panel-body">
-          <form role="form">
-            <div class="form-group">
-              <label for="exampleInputEmail1">Email</label>
-              <input type="email" class="form-control" id="email1" placeholder="Enter email" required>
-            </div>
-            <div class="form-group">
-              <label for="exampleInputPassword1">Contraseña</label>
-              <input type="password" class="form-control" id="password1" placeholder="Password" required>
-            </div>
-            <div class="form-group">
-              <label for="exampleInputPassword1">Repetir Contraseña</label>
-              <input type="password" class="form-control" id="password2" placeholder="Password" required>
-            </div>
-            <div class="text-right">
-              <br>
-              <button type="submit" class="btn btn-sm btn-change">Cambiar contraseña</button>
-            </div>
-          </form>
-        </div>
+      <ResetPasswordForm v-if="validate"></ResetPasswordForm>
+      <div v-else="validate">
+        <p class="lead">
+          El tiempo establecido para cambiar la contrase ha experido.
+          Por favor vuelve a solicitar el cambio de contraseña dentro de la app Mobi.
+        </p>
       </div>
     </div>
     
@@ -35,12 +17,33 @@
 </template>
 
 <script>
+  import axios from 'axios';
+  
+  import ResetPasswordForm from './ResetPasswordForm';
+
   export default {
     name: 'container',
+    components: {
+      ResetPasswordForm,
+    },
     data() {
       return {
         msg: 'Mobi App',
+        validate: true,
       };
+    },
+    methods: {
+      validateToken(token) {
+        const self = this;
+        axios
+          .get(`http://localhost:3030/api/users/reset/${token}`)
+          .then((res) => { self.validate = res.data.validate; })
+          .catch(() => { self.validate = false; });
+      },
+    },
+    mounted() {
+      const token = this.$route.params.token;
+      this.validateToken(token);
     },
   };
 
@@ -52,33 +55,8 @@
     font-weight: normal;
     text-align: center;
   }
-  .form--container{
+  .lead{
     margin-top: 30px;
-  }
-  .btn-change {
-    color: #fff;
-    background-color: #d47729;
-    border-color: #d47729;
-  }
-  .btn {
-    display: inline-block;
-    padding: 6px 12px;
-    margin-bottom: 0;
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 1.42857143;
-    text-align: center;
-    white-space: nowrap;
-    vertical-align: middle;
-    -ms-touch-action: manipulation;
-    touch-action: manipulation;
-    cursor: pointer;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-    background-image: none;
-    border: 1px solid transparent;
-    border-radius: 4px;
+    text-align: justify;
   }
 </style>
---
+++ src/components/ResetPasswordForm.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title"><strong>Ingresa aquí una nueva para el acceso a tu aplicación</strong></h3>
+    </div>
+    <div class="panel-body">
+      <form role="form">
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email</label>
+          <input type="email" class="form-control" id="email1" placeholder="Enter email" required>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Contraseña</label>
+          <input type="password" class="form-control" id="password1" placeholder="Password" required>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Repetir Contraseña</label>
+          <input type="password" class="form-control" id="password2" placeholder="Password" required>
+        </div>
+        <div class="text-right">
+          <br>
+          <button type="submit" class="btn btn-sm btn-change">Cambiar contraseña</button>
+        </div>
+      </form>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'resetPasswordForm',
+    data() {
+      return {
+        msg: 'Mobi App',
+      };
+    },
+  };
+
+</script>
+
+<style scoped>
+  .form--container{
+    margin-top: 30px;
+  }
+  .btn-change {
+    color: #fff;
+    background-color: #d47729;
+    border-color: #d47729;
+  }
+  .btn {
+    display: inline-block;
+    padding: 6px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.42857143;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    -ms-touch-action: manipulation;
+    touch-action: manipulation;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    background-image: none;
+    border: 1px solid transparent;
+    border-radius: 4px;
+  }
+</style>
